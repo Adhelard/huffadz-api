382f19e45b85ec7a83b61b62a9d8e7062fcca0f1
aleefin <blackarifin15@gmail.com>
2025-11-09 20:14:54 +0700

    ubah dikit

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://huffadz.portalsi.com",     
     ],
     allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
